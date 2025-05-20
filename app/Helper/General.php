38891a1ced6f8b78656c0e5c2b7881ee95524fda
Foster Amponsah <foster.asante@gwosevo.com>
2025-05-20 15:29:19 +0000

    Update

--- app/Helper/General.php
+++ app/Helper/General.php
@@ -4,6 +4,15 @@ namespace App\Helper;
 
 class General {
     public static function validateMsisdn(){
-        
+
+    }
+
+    public static function apiResponse(bool $messageType, string $message, $data)
+    {
+        return response()->json([
+            'success' => $messageType,
+            'message' => $message,
+            'data' => $data
+        ]);
     }
 }
