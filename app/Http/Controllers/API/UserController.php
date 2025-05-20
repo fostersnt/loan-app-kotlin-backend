59696d250f9e71402faf43c137c81bece33a235b
Foster Amponsah <foster.asante@gwosevo.com>
2025-05-20 11:55:16 +0000

    Update

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -13,5 +14,23 @@ class UserController extends Controller
         $validator = Validator::make($request->all(), [
             'msisdn' => 'required|string|max:12'
         ]);
+    }
+
+    public function edit(Request $request, $user_id)
+    {
+        $validator = Validator::make($request->all(), [
+            'msisdn' => 'required|string|max:12'
+        ]);
+
+        $user = User::query()->find($user_id);
+
+        if ($user) {
+            $user->update([
+                'msisdn' => $request->msisdn
+            ]);
+        } else {
+            
+        }
+        
     }
 }
