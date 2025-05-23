997f4a59cad710f0105fbd42879c824945afb517
Foster Amponsah <foster.asante@gwosevo.com>
2025-05-23 10:48:12 +0000

    Update

--- resources/js/Pages/Charts/DoughnutChart.jsx
+++ resources/js/Pages/Charts/DoughnutChart.jsx
@@ -5,7 +5,7 @@ import '../../../css/charts.css'
 import React from 'react'
 import { color } from "chart.js/helpers";
 
-export default function DoughnutChart() {
+export default function DoughnutChart({dataset_1, dataset_2}) {
     ChartJS.register(ArcElement, Tooltip, Legend);
     return (
         <div className="chartContainer">
@@ -15,23 +15,26 @@ export default function DoughnutChart() {
                     labels: ['Jun', 'Jul', 'Aug'],
                     labels: ['Jun', 'Jul', 'Aug'],
                     datasets: [
-                        {
-                            id: 1,
-                            label: '',
-                            data: [5, 6, 7],
-                            backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-                            borderColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-                            borderWidth: 1,
-                        },
-                        {
-                            id: 2,
-                            label: '',
-                            data: [3, 2, 1],
-                            backgroundColor: ["#4BC0C0", "#FF9F40", "#FFCD56"],
-                            borderColor: ["#4BC0C0", "#FF9F40", "#FFCD56"],
-                            borderWidth: 1
-                        },
+                        dataset_1, dataset_2
                     ],
+                    // datasets: [
+                    //     {
+                    //         id: 1,
+                    //         label: '',
+                    //         data: [5, 6, 7],
+                    //         backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+                    //         borderColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+                    //         borderWidth: 1,
+                    //     },
+                    //     {
+                    //         id: 2,
+                    //         label: '',
+                    //         data: [3, 2, 1],
+                    //         backgroundColor: ["#4BC0C0", "#FF9F40", "#FFCD56"],
+                    //         borderColor: ["#4BC0C0", "#FF9F40", "#FFCD56"],
+                    //         borderWidth: 1
+                    //     },
+                    // ],
                 }}
                 fallbackContent={<div><h1>HELLO DOUGHNUT CHART</h1></div>}
             />
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -4,10 +4,28 @@ import AuthLayout from '../Layouts/AuthLayout';  // Ensure this path is correct
 import DoughnutChart from './Charts/DoughnutChart';
 
 const Home = ({ userName }) => {
+  const data1 = {
+    id: 1,
+    label: '',
+    data: [5, 6, 7],
+    backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+    borderColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+    borderWidth: 1,
+  };
+
+  const data2 = {
+    id: 2,
+    label: '',
+    data: [3, 2, 1],
+    backgroundColor: ["#4BC0C0", "#FF9F40", "#FFCD56"],
+    borderColor: ["#4BC0C0", "#FF9F40", "#FFCD56"],
+    borderWidth: 1
+  };
+
   return (
     <div>
       <h1>Home Page Here</h1>
-      <DoughnutChart />
+      <DoughnutChart dataset_1={ data1 } dataset_2={ data2 } />
     </div>
   );
 };
