b6ccb0c74940f5b16fd1c5532b8d2050d5a48fd9
Foster Amponsah <foster.asante@gwosevo.com>
2025-05-23 12:32:40 +0000

    Update

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -34,12 +34,13 @@ class Handler extends ExceptionHandler
     {
          // Check if the request expects an Inertia response
         if ($request->expectsJson()) {
-            Log::channel('exceptions')->info("MESSAGE === " . $e->getMessage());
+            Log::info("MESSAGE-JSON [" . $e->getMessage() . "], REQUEST [" . json_encode($request) . "]");
             return parent::render($request, $e);
         }
 
         // Handle 404 or other errors and display a custom React error page
         if ($e instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
+            Log::info("MESSAGE-HTML-A [" . $e->getMessage() . "], REQUEST [" . json_encode($request) . "]");
             return Inertia::render('ErrorPage', [
                 'statusCode' => 404,
                 'message' => 'Page not found.',
@@ -47,6 +48,7 @@ class Handler extends ExceptionHandler
         }
 
         if ($e instanceof \Illuminate\Database\QueryException) {
+            Log::info("MESSAGE-HTML-B [" . $e->getMessage() . "], REQUEST [" . json_encode($request) . "]");
             return Inertia::render('ErrorPage', [
                 'statusCode' => 500,
                 'message' => 'Database error.',
