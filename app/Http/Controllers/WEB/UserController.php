11f15131414ae427fb10e7b592e5cc78b39debc3
Foster Amponsah <foster.asante@gwosevo.com>
2025-05-23 10:03:56 +0000

    Update

--- app/Http/Controllers/WEB/UserController.php
+++ app/Http/Controllers/WEB/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\WEB;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -11,6 +12,8 @@ class UserController extends Controller
     public function index()
     {
         sleep(1);
+        $user = User::query()->first();
+        dd($user->only('id', 'name'));
         return Inertia::render('User/ViewUsers', ['userData' => 'Foster Asante']);
     }
 
