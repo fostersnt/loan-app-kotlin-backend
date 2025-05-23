07a4c3bf517f414177bc70239850f6969f1db982
Foster Amponsah <foster.asante@gwosevo.com>
2025-05-23 15:22:14 +0000

    Update

--- resources/js/Pages/Charts/DoughnutChart.jsx
+++ resources/js/Pages/Charts/DoughnutChart.jsx
@@ -2,9 +2,6 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 import '../../../css/charts.css'
 
-import React from 'react'
-import { color } from "chart.js/helpers";
-
 export default function DoughnutChart({dataset_1, dataset_2}) {
     ChartJS.register(ArcElement, Tooltip, Legend);
     return (
@@ -15,25 +12,7 @@ export default function DoughnutChart({dataset_1, dataset_2}) {
                     labels: ['Jun', 'Jul', 'Aug'],
                     datasets: [
                         dataset_1, dataset_2
-                    ],
-                    // datasets: [
-                    //     {
-                    //         id: 1,
-                    //         label: '',
-                    //         data: [5, 6, 7],
-                    //         backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-                    //         borderColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-                    //         borderWidth: 1,
-                    //     },
-                    //     {
-                    //         id: 2,
-                    //         label: '',
-                    //         data: [3, 2, 1],
-                    //         backgroundColor: ["#4BC0C0", "#FF9F40", "#FFCD56"],
-                    //         borderColor: ["#4BC0C0", "#FF9F40", "#FFCD56"],
-                    //         borderWidth: 1
-                    //     },
-                    // ],
+                    ]
                 }}
                 options={{
                     responsive: true,
