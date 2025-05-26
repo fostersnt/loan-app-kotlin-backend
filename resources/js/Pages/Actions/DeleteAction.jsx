8d6dd0cc99e2c35337f97fcf80a5acd49fa4048f
Foster Amponsah <foster.asante@gwosevo.com>
2025-05-26 13:21:44 +0000

    Update

--- resources/js/Pages/Actions/DeleteAction.jsx
+++ resources/js/Pages/Actions/DeleteAction.jsx
@@ -3,7 +3,7 @@ import { Trash2 } from 'lucide-react'; // or use any icon library you prefer
 const DeleteAction = ({ action, label }) => {
     return (
         <button onClick={action} style={{border: 'none', backgroundColor: 'transparent', cursor: 'pointer'}}>
-            <Trash2 strokeWidth={1} />
+            <Trash2 strokeWidth={1} color='#FF6384' />
         </button>
     );
 };
--- resources/js/Pages/Actions/EditAction.jsx
+++ resources/js/Pages/Actions/EditAction.jsx
@@ -3,7 +3,7 @@ import { SquarePen } from 'lucide-react'; // or use any icon library you prefer
 const EditAction = ({ action, label }) => {
     return (
         <button onClick={action} style={{border: 'none', backgroundColor: 'transparent', cursor: 'pointer'}}>
-            <SquarePen strokeWidth={1} />
+            <SquarePen strokeWidth={1} color='#34495e' />
         </button>
     );
 };
