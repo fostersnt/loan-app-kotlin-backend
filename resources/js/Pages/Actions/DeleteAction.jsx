a8c399158f26be0c334d1dc4363ba73b69b3ca32
Foster Amponsah <foster.asante@gwosevo.com>
2025-05-26 13:20:12 +0000

    Updated stroke width

--- resources/js/Pages/Actions/DeleteAction.jsx
+++ resources/js/Pages/Actions/DeleteAction.jsx
@@ -3,7 +3,7 @@ import { Trash2 } from 'lucide-react'; // or use any icon library you prefer
 const DeleteAction = ({ action, label }) => {
     return (
         <button onClick={action} style={{border: 'none', backgroundColor: 'transparent', cursor: 'pointer'}}>
-            <Trash2 style={{}} />
+            <Trash2 strokeWidth={1} />
         </button>
     );
 };
--- resources/js/Pages/Actions/EditAction.jsx
+++ resources/js/Pages/Actions/EditAction.jsx
@@ -3,7 +3,7 @@ import { SquarePen } from 'lucide-react'; // or use any icon library you prefer
 const EditAction = ({ action, label }) => {
     return (
         <button onClick={action} style={{border: 'none', backgroundColor: 'transparent', cursor: 'pointer'}}>
-            <SquarePen style={{}} />
+            <SquarePen strokeWidth={1} />
         </button>
     );
 };
