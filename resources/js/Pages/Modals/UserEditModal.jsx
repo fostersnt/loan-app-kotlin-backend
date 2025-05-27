decca8ef4cd9afaeadbf49f8a74957b00b51d239
Foster Amponsah <foster.asante@gwosevo.com>
2025-05-27 11:01:13 +0000

    Updated the modal design

--- resources/js/Pages/Modals/UserEditModal.jsx
+++ resources/js/Pages/Modals/UserEditModal.jsx
@@ -1,19 +1,18 @@
-// resources/js/Components/Modal.jsx
-import React from 'react';
+import '../../../css/modals.css'
 
 export default function UserEditModal({ show, onClose, children }) {
   if (!show) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white p-6 rounded shadow-lg relative">
-        <button
-          className="absolute top-2 right-2 text-gray-600"
-          onClick={onClose}
-        >
-          &times;
-        </button>
-        {children}
+    <div className="modal-backdrop">
+      <div className="modal-box">
+        <div className="modal-header">
+          <h5 className="modal-title">Modal Title</h5>
+          <button className="modal-close" onClick={onClose}>&times;</button>
+        </div>
+        <div className="modal-body">
+          {children}
+        </div>
       </div>
     </div>
   );
