bf1aca472b6d9f202ea47c3979cb6c0e9f128e6a
Foster Amponsah <foster.asante@gwosevo.com>
2025-05-27 17:12:55 +0000

    Update

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,6 +1,14 @@
 import './bootstrap';
 import { createInertiaApp } from '@inertiajs/react'
+import NProgress from 'nprogress';
 import { createRoot } from 'react-dom/client'
+
+// NProgress.configure({
+//   delay: 250,          // Delay before showing the progress bar (in ms)
+//   color: '#FF6384',    // Color of the progress bar
+//   includeCSS: true,    // Include default NProgress CSS
+//   showSpinner: false,  // Disable the spinner (can be enabled if desired)
+// });
 
 createInertiaApp({
   resolve: name => {
@@ -13,6 +21,17 @@ createInertiaApp({
     createRoot(el).render(<App {...props} />)
   },
   //Progress bar configuration below
+  // progress: {
+  //   onStart: () => {
+  //     NProgress.start();  // Start the progress bar when the request starts
+  //   },
+  //   onFinish: () => {
+  //     NProgress.done();   // Complete the progress bar when the request finishes
+  //   },
+  //   onError: () => {
+  //     NProgress.done();   // Complete the progress bar if there's an error
+  //   },
+  // },
   progress: {
     delay: 250,
 
@@ -23,6 +42,6 @@ createInertiaApp({
     includeCSS: true,
 
     // Whether the NProgress spinner will be shown...
-    showSpinner: false,
+    showSpinner: true,
   },
 })
