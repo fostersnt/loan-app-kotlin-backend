0eadfb3e4eff5f15c057f8f21d2cc977369ab082
Foster Amponsah <foster.asante@gwosevo.com>
2025-05-28 09:45:14 +0000

    Update

--- resources/js/Pages/Exports/ExcelExport.jsx
+++ resources/js/Pages/Exports/ExcelExport.jsx
@@ -2,12 +2,14 @@ import * as XLSX from 'xlsx';
 import { saveAs } from 'file-saver';
 
 export default function ExcelExport (data, fileName) {
+
+  const filteredData = data.map(({id, msisdn, ...remainingData}) => remainingData)
   // const data = [
   //   { name: 'John', age: 30 },
   //   { name: 'Jane', age: 25 },
   // ];
 
-  const worksheet = XLSX.utils.json_to_sheet(data);
+  const worksheet = XLSX.utils.json_to_sheet(filteredData);
   const workbook = XLSX.utils.book_new();
   XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
 
